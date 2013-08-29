7bbf77305033b461b414a543c234bf53a6ea7b50
wendy <wendy@dy-wen.(none)>
2013-08-29 14:18:03 +0200

    some re-reorganisation in the folder layers!

---
+++ layers/lidia/lidia-test.sh
@@ -0,0 +1,11 @@
+for index in {1..15}
+do
+  echo "\".......//////.......//////.......//////.......//////.......//////.......//////.......\","
+  echo "\"o.....////////.....////////.....////////.....////////.....////////.....////////.....o\","
+  echo "\"oo...//////////...//////////...//////////...//////////...//////////...//////////...oo\","
+  echo "\"ooo.////////////.////////////.////////////.////////////.////////////.////////////.ooo\","
+  echo "\"oooollllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllooooo\","
+  echo "\"oooollllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllooooo\","
+  echo "\"oooollllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllnnnnllllllooooo\","
+done
+
